f846bfd100bb1ca6e4074fc4b40a026447761020
Subrat Kumar Pattanaik <subratpatnaik.email@gmail.com>
2023-03-03 16:17:23 +0530

    cors changes

--- app.py
+++ app.py
@@ -1,15 +1,19 @@
 from flask import Flask
-from flask_restful import Resource, Api 
+from flask_restful import Resource, Api
 from resources.api.pdf_search import PDFSearch
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 api = Api(app)
+
 
 @app.route("/")
 def welcome():
     return "<p>This is a PDF search service API implemented in Flask</p>"
 
+
 api.add_resource(PDFSearch, "/api/pdf-search")
 
-if __name__=="__main__":
+if __name__ == "__main__":
     app.run(port=5000)
--- resources/api/pdf_search.py
+++ resources/api/pdf_search.py
@@ -7,11 +7,12 @@ class PDFSearch(Resource):
     def get(self):
         search_term = str(request.args["search-term"]).strip()
         pdf_path = r"{}".format(request.args["pdf-abs-path"]).strip()
+        print(pdf_path)
         if ',' in search_term:
-            search_terms = list(map(lambda search_term: search_term.strip(), search_term.split(',')))
+            search_terms = list(
+                map(lambda search_term: search_term.strip(), search_term.split(',')))
             return pdf_searcher.search_for_multiple(search_terms, pdf_path), 200
         return pdf_searcher.search_for(search_term, pdf_path), 200
-            
 
     def post(self):
         pass
